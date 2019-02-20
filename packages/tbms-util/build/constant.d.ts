e4ae96b5b79468864ff0c110d275bae5ae41842b
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-20 10:29:55 +0800

    feat: add packages module

---
+++ packages/tbms-util/build/constant.d.ts
@@ -0,0 +1,15 @@
+declare const MSG_EVENT_CONSTANT: {
+    KICK_OFF: string;
+    LOGIN_ERROR: string;
+    LOGIN_SUCCESS: string;
+    RECEIVE_MSG: string;
+    RECEIVE_MSG_ERROR: string;
+    SEND_MSG_SUCCESS: string;
+    SEND_MSG_ERROR: string;
+    GET_OFFLINE_MSG: string;
+    GET_OFFLINE_MSG_ERROR: string;
+    HEAT_ERROR: string;
+    NETWORK_CLOSE: string;
+    NETWORK_RECONNECT: string;
+};
+export { MSG_EVENT_CONSTANT };
