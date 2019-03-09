616fc3073699164e65c352ca68432c5e6dc10a4c
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-03-10 00:01:27 +0800

    feat: add log module

--- packages/tbms-util/build/index.d.ts
+++ packages/tbms-util/build/index.d.ts
@@ -3,4 +3,5 @@ import debug from './log';
 import * as _ from './util';
 import Middleware from './middleware';
 import * as Constant from './constant';
-export { EventEmitter, debug, _, Middleware, Constant };
+import log from './log';
+export { EventEmitter, debug, _, Middleware, Constant, log };
--- packages/tbms-util/src/index.ts
+++ packages/tbms-util/src/index.ts
@@ -3,11 +3,13 @@ import debug from './log';
 import * as _ from './util';
 import Middleware from './middleware';
 import * as Constant from './constant';
+import log from './log';
 
 export {
   EventEmitter,
   debug,
   _,
   Middleware,
-  Constant
+  Constant,
+  log
 }
